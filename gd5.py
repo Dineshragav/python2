24f734a36b73a3f8256865441b8b1b356455d835
Dineshragav <52067784+Dineshragav@users.noreply.github.com>
2019-07-03 15:29:05 +0530

    Create gd5.py

---
+++ gd5.py
@@ -0,0 +1,10 @@
+a=int(input("First number:"))
+b=int(input("Second number:"))
+c=int(input("Third number:"))
+if (a>b) and (a>c):
+  largest=a
+elif (b>a) and (b>c):
+  largest=b
+else:
+  largest=c
+  
