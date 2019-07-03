9975153a6c868d5a5eca714fd1a1471e92235492
Dineshragav <52067784+Dineshragav@users.noreply.github.com>
2019-07-03 12:09:31 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
-din=input()
-if din=='a' or din=='e' or din=='i' or din=='o' or din=='u' or din=='A' or din=='E' or din=='I' din=='O' din=='U':
-  print("vowel")
+din=str(input(""))
+if(din=='a' or'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' 'O' 'U'):
+  print("Vowel")
 else:
-  print("consonent")
+  print("Consonant")
